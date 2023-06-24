f06a30013e49c2a228d08ca5dd312030f8751658
herzblutnord <v.schecklein@gmail.com>
2023-06-24 23:29:48 +0200

    put url parsing in a seperate class

--- src/main/java/moe/herz/Musicbot.java
+++ src/main/java/moe/herz/Musicbot.java
@@ -4,14 +4,10 @@ import java.util.regex.Pattern;
 import java.io.IOException;
 import javax.net.ssl.SSLSocketFactory;
 import java.util.regex.Matcher;
-import java.net.URISyntaxException;
-import java.net.URI;
 import java.io.FileInputStream;
 import java.util.Properties;
 import java.sql.Connection;
 import java.sql.*;
-
-import java.net.HttpURLConnection;
 
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
@@ -22,13 +18,10 @@ import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.User;
 import org.pircbotx.hooks.events.InviteEvent;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
 public class Musicbot extends ListenerAdapter {
-    private static final String botName = "Undine"; // Moved botName here, and made it static
+    private static final String botName = "UndineWIP";
+    private static final String botVersion = "0.5.1";
+
     private YoutubeService youtubeService;
     private LastFmService lastFmService;
     private TellMessageHandler tellMessageHandler;
@@ -84,11 +77,10 @@ public class Musicbot extends ListenerAdapter {
     public static void main(String[] args) {
 
         // Configure the bot
-
         Configuration configuration = new Configuration.Builder()
                 .setName(botName)
                 .addServer("", 6697)
-                .addAutoJoinChannel("#musicbottesting")
+                .addAutoJoinChannel("#musicbottesting2")
                 .addListener(new Musicbot())
                 .setSocketFactory(SSLSocketFactory.getDefault()) // Enable SSL
                 .buildConfiguration();
@@ -106,35 +98,7 @@ public class Musicbot extends ListenerAdapter {
     public void onJoin(JoinEvent event) {
         User user = event.getUser();
         if (user != null && user.getNick().equals(botName)) {
-            event.getChannel().send().message("Greetings from the depths, I'm " + botName + ", your helpful water spirit! (Version 0.5)");
-        }
-    }
-
-    // Method to fetch website metadata
-    public static String fetchWebsiteMetadata(String url) {
-        try {
-            URI uri = new URI(url); // Check that it's a valid URL
-            try {
-                // Send an HTTP GET request
-                HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
-                connection.setRequestMethod("GET");
-                connection.connect();
-
-                // Parse the HTML content
-                Document doc = Jsoup.parse(connection.getInputStream(), null, url);
-                return doc.title();
-            } catch (IOException e) {
-                // If the GET request fails, fall back to HtmlUnit
-                try (final WebClient webClient = new WebClient()) {
-                    webClient.getOptions().setJavaScriptEnabled(false);  // Disable JavaScript
-                    final HtmlPage page = webClient.getPage(url);
-                    return page.getTitleText();
-                }
-            }
-        } catch (URISyntaxException e) {
-            return "Invalid URL";
-        } catch (Exception e) {
-            return "Error connecting to URL";
+            event.getChannel().send().message("Greetings from the depths, I'm " + botName + ", your helpful water spirit! (Version "+ botVersion+ ")");
         }
     }
 
@@ -144,7 +108,6 @@ public class Musicbot extends ListenerAdapter {
 
         // Regular expression pattern to identify URLs in the message
         Pattern urlPattern = Pattern.compile("(https?://[\\w.-]+\\.[\\w.-]+[\\w./?=&#%\\-\\(\\)]*)", Pattern.CASE_INSENSITIVE);
-
         Matcher matcher = urlPattern.matcher(event.getMessage());
 
         if (event.getMessage().startsWith(".np ")) {
@@ -203,8 +166,8 @@ public class Musicbot extends ListenerAdapter {
                     }
 
                     if (!skip) {
-
-                        String metadata = fetchWebsiteMetadata(url);
+                        // Use the UrlMetadataFetcher class to get the metadata
+                        String metadata = UrlMetadataFetcher.fetchWebsiteMetadata(url);
                         event.respondWith(metadata);
                     }
                 }
