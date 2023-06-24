2cdd05f681207d422c73c1b4cd8ab7d0ae978bb9
herzblutnord <v.schecklein@gmail.com>
2023-06-25 00:09:20 +0200

    auferstanden aus Ruinen

--- src/main/java/moe/herz/Musicbot.java
+++ src/main/java/moe/herz/Musicbot.java
@@ -1,13 +1,9 @@
 package moe.herz;
 
+import java.sql.SQLException;
 import java.util.regex.Pattern;
-import java.io.IOException;
-import javax.net.ssl.SSLSocketFactory;
 import java.util.regex.Matcher;
-import java.io.FileInputStream;
-import java.util.Properties;
-import java.sql.Connection;
-import java.sql.*;
+import javax.net.ssl.SSLSocketFactory;
 
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
@@ -19,73 +15,35 @@ import org.pircbotx.User;
 import org.pircbotx.hooks.events.InviteEvent;
 
 public class Musicbot extends ListenerAdapter {
-    private static final String botName = "UndineWIP";
-    private static final String botVersion = "0.5.1";
+    private static final String BOT_NAME = "UndineWIP";
+    private static final String BOT_VERSION = "0.5.2";
 
-    private YoutubeService youtubeService;
-    private LastFmService lastFmService;
-    private TellMessageHandler tellMessageHandler;
-    private Properties properties;
+    private final YoutubeService youtubeService;
+    private final LastFmService lastFmService;
+    private final TellMessageHandler tellMessageHandler;
 
-    // Database connection
-    private Connection db;
-
-    public void loadProperties() {
-        try (FileInputStream in = new FileInputStream("./config2.properties")) {
-            properties = new Properties();
-            properties.load(in);
-
-            // Connect to database
-            String databaseURL = properties.getProperty("db.url");
-            Connection conn = null;
-            try {
-                conn = DriverManager.getConnection(databaseURL, properties);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-            db = conn;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public Musicbot(YoutubeService youtubeService, LastFmService lastFmService, TellMessageHandler tellMessageHandler) {
+        this.youtubeService = youtubeService;
+        this.lastFmService = lastFmService;
+        this.tellMessageHandler = tellMessageHandler;
     }
 
-    public String getProperty(String key) {
-        return properties.getProperty(key);
-    }
+    public static void main(String[] args) throws SQLException {
+        Config config = new Config();
+        YoutubeService youtubeService = new YoutubeService(config);
+        LastFmService lastFmService = new LastFmService(config);
+        TellMessageHandler tellMessageHandler = new TellMessageHandler(config.getDbConnection());
 
-    public Musicbot() {
-        loadProperties();
-        try {
-            youtubeService = new YoutubeService(this);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-             lastFmService = new LastFmService(this);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            tellMessageHandler = new TellMessageHandler(db);
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+        Musicbot botInstance = new Musicbot(youtubeService, lastFmService, tellMessageHandler);
 
-    public static void main(String[] args) {
-
-        // Configure the bot
         Configuration configuration = new Configuration.Builder()
-                .setName(botName)
+                .setName(BOT_NAME)
                 .addServer("", 6697)
                 .addAutoJoinChannel("#musicbottesting2")
-                .addListener(new Musicbot())
-                .setSocketFactory(SSLSocketFactory.getDefault()) // Enable SSL
+                .addListener(botInstance)
+                .setSocketFactory(SSLSocketFactory.getDefault())
                 .buildConfiguration();
 
-        // Start the bot
         try (PircBotX bot = new PircBotX(configuration)) {
             bot.startBot();
         } catch (Exception e) {
@@ -93,83 +51,80 @@ public class Musicbot extends ListenerAdapter {
         }
     }
 
-    // Greeting on bot joining
     @Override
     public void onJoin(JoinEvent event) {
         User user = event.getUser();
-        if (user != null && user.getNick().equals(botName)) {
-            event.getChannel().send().message("Greetings from the depths, I'm " + botName + ", your helpful water spirit! (Version "+ botVersion+ ")");
+        if (user != null && user.getNick().equals(BOT_NAME)) {
+            event.getChannel().send().message("Greetings from the depths, I'm " + BOT_NAME + ", your helpful water spirit! (Version " + BOT_VERSION + ")");
         }
     }
 
-    // Method to handle general message events
     @Override
     public void onGenericMessage(GenericMessageEvent event) {
-
-        // Regular expression pattern to identify URLs in the message
+        String message = event.getMessage();
         Pattern urlPattern = Pattern.compile("(https?://[\\w.-]+\\.[\\w.-]+[\\w./?=&#%\\-\\(\\)]*)", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = urlPattern.matcher(event.getMessage());
+        Matcher matcher = urlPattern.matcher(message);
 
-        if (event.getMessage().startsWith(".np ")) {
-            String username = event.getMessage().substring(4);
-            try {
-                String response = lastFmService.getCurrentTrack(username);
-                event.respondWith(response);
-            } catch (Exception e) {
-                e.printStackTrace();
-                event.respondWith("Error retrieving last.fm data");
-            }
-        }
-
-        else if (event.getMessage().startsWith(".yt ")) {
-            String query = event.getMessage().substring(4);
-            String videoUrl = youtubeService.searchYoutube(query);
-            if (videoUrl != null) {
-                event.respondWith(videoUrl);
-            }
+        if (message.startsWith(".np ")) {
+            handleNowPlayingCommand(event, message);
+        } else if (message.startsWith(".yt ")) {
+            handleYoutubeCommand(event, message);
         } else {
-            // Find urls in the message
-            while (matcher.find()) {
-                String url = matcher.group(1);
+            handleUrlFetching(event, matcher);
+        }
+    }
 
-                // Trimming trailing text (if any) from the URL
-                int spaceIndex = url.indexOf(' ');
-                if (spaceIndex != -1) {
-                    url = url.substring(0, spaceIndex);
+    private void handleNowPlayingCommand(GenericMessageEvent event, String message) {
+        String username = message.substring(4);
+        try {
+            String response = lastFmService.getCurrentTrack(username);
+            event.respondWith(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleYoutubeCommand(GenericMessageEvent event, String message) {
+        String query = message.substring(4);
+        String videoUrl = youtubeService.searchYoutube(query);
+        if (videoUrl != null) {
+            event.respondWith(videoUrl);
+        }
+    }
+
+    private void handleUrlFetching(GenericMessageEvent event, Matcher matcher) {
+
+        while (matcher.find()) {
+            String url = matcher.group(1);
+            int spaceIndex = url.indexOf(' ');
+            if (spaceIndex != -1) {
+                url = url.substring(0, spaceIndex);
+            }
+
+            // For youtube.com/watch?v= and youtu.be/ links, get and send video details
+            if (url.contains("youtube.com/watch?v=") || url.contains("youtu.be/")) {
+                String videoId = url.contains("youtube.com/watch?v=") ? url.substring(url.indexOf("=") + 1) : url.substring(url.indexOf("be/") + 3);
+                String videoDetails = youtubeService.getVideoDetails(videoId);
+                if (videoDetails != null) {
+                    if (event instanceof MessageEvent messageEvent) {
+                        messageEvent.getBot().sendIRC().message(messageEvent.getChannel().getName(), videoDetails);
+                    }
+                }
+            } else {
+                // Skip non-HTML files
+                String[] skippedExtensions = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp", ".mp4", ".mp3", ".wav", ".ogg", ".flac", ".mkv", ".avi", ".flv"};
+                boolean skip = false;
+                for (String extension : skippedExtensions) {
+                    if (url.toLowerCase().endsWith(extension)) {
+                        skip = true;
+                        break;
+                    }
                 }
 
-                if (url.contains("youtube.com/watch?v=")) {
-                    String videoId = url.substring(url.indexOf("=") + 1);
-                    String videoDetails = youtubeService.getVideoDetails(videoId);
-                    if (videoDetails != null) {
-                        if (event instanceof MessageEvent messageEvent) {
-                            messageEvent.getBot().sendIRC().message(messageEvent.getChannel().getName(), videoDetails);
-                        }
-                    }
-                } else if (url.contains("youtu.be/")) {
-                    String videoId = url.substring(url.indexOf("be/") + 3);
-                    String videoDetails = youtubeService.getVideoDetails(videoId);
-                    if (videoDetails != null) {
-                        if (event instanceof MessageEvent messageEvent) {
-                            messageEvent.getBot().sendIRC().message(messageEvent.getChannel().getName(), videoDetails);
-                        }
-                    }
-                } else {
-                    // Skip non-HTML files
-                    String[] skippedExtensions = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp", ".mp4", ".mp3", ".wav", ".ogg", ".flac", ".mkv", ".avi", ".flv"};
-                    boolean skip = false;
-                    for (String extension : skippedExtensions) {
-                        if (url.toLowerCase().endsWith(extension)) {
-                            skip = true;
-                            break;
-                        }
-                    }
-
-                    if (!skip) {
-                        // Use the UrlMetadataFetcher class to get the metadata
-                        String metadata = UrlMetadataFetcher.fetchWebsiteMetadata(url);
-                        event.respondWith(metadata);
-                    }
+                if (!skip) {
+                    // Use the UrlMetadataFetcher class to get the metadata
+                    String metadata = UrlMetadataFetcher.fetchWebsiteMetadata(url);
+                    event.respondWith(metadata);
                 }
             }
         }
@@ -192,10 +147,8 @@ public class Musicbot extends ListenerAdapter {
         if (user == null) {
             return;
         }
-
         String messageText = event.getMessage();
         String sender = user.getNick();
-
         if (messageText.startsWith(".tell")) {
             tellMessageHandler.handleTellMessage(sender, messageText, event);
         } else {
